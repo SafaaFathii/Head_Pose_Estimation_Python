bd4399d75c9ad58043e82e02ac07691d9b191855
SafaaFathii <safaafathii123@gmail.com>
2022-05-19 15:23:10 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Head_Pose_Estimation_Project_End_to_End',
+    author='Safaa Fathii',
+    license='',
+)
